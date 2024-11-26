52f5c5ce93152b8e2a5a5eea8421bb107250c4d2
codinci <dkibau@gmail.com>
2024-11-26 21:20:51 +0300

    page updates

--- script.js
+++ script.js
@@ -8,32 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-let currentIndex = 0;
-const slides = document.querySelectorAll(".client-slide");
-const totalSlides = slides.length;
-
-function showSlide(index) {
-  const slideWidth = slides[0].clientWidth;
-  const offset = -index * slideWidth;
-  document.querySelector(
-    ".client-slider"
-  ).style.transform = `translateX(${offset}px)`;
-}
-
-document.querySelector(".prev").addEventListener("click", () => {
-  currentIndex = currentIndex > 0 ? currentIndex - 1 : totalSlides - 1;
-  showSlide(currentIndex);
-});
-
-document.querySelector(".next").addEventListener("click", () => {
-  currentIndex = currentIndex < totalSlides - 1 ? currentIndex + 1 : 0;
-  showSlide(currentIndex);
-});
-
-window.addEventListener("resize", () => {
-  showSlide(currentIndex); // Adjust the position on window resize
-});
-
 
 // Set current year in the footer
 document.getElementById("current-year").textContent = new Date().getFullYear();
